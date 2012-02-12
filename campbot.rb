0d15e783058dbfab98189574e0419d78d3a46fa5
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-12 13:46:03 -0500

    Make regex case insensitive.

--- campbot.rb
+++ campbot.rb
@@ -8,8 +8,8 @@ class CampBot < Sinatra::Base
     payload = JSON.parse(params[:payload])
     halt 400, '' if payload['repository']['url'] != ENV['GITHUB_REPO']
     payload['commits'].each do |commit|
-      puts "Commit = #{commit.inspect}"
-      if commit['message'] =~ /(fixes?|closes?) (\d+)/
+      puts "Commit message = #{commit['message'].inspect}"
+      if commit['message'] =~ /(fixes?|closes?) (\d+)/i
         puts "Matching commit."
         t = Basecamp::TodoItem.find($1)
         t.complete!
