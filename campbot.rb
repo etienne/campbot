5f4c84c7ebd4cb8b2d073cbaafd1b78536860d2b
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-12 15:14:36 -0500

    Add message to todo item.

--- campbot.rb
+++ campbot.rb
@@ -8,11 +8,12 @@ class CampBot < Sinatra::Base
     payload = JSON.parse(params[:payload])
     halt 400, '' if payload['repository']['url'] != ENV['GITHUB_REPO']
     payload['commits'].each do |commit|
-      puts "Commit message = #{commit['message'].inspect}"
       if commit['message'] =~ /(?:fix|fixes|closes?) (\d+)/i
-        puts "Matching commit, TodoItem id = #{$1}"
-        t = Basecamp::TodoItem.find($1)
+        todo_item_id = $1
+        t = Basecamp::TodoItem.find(todo_item_id)
         t.complete!
+        comment = "Completed by #{commit['author']['name']} in commit <a href=\"#{commit['url']}\">#{commit['id'][0..9]}</a>"
+        Basecamp::Comment.create(:todo_item_id => todo_item_id, :contents => comment)
       end
     end
   end
