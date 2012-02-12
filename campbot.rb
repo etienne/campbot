7ce869c8c2096594d1504b826021691f82db80ae
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-12 12:48:07 -0500

    Debug stuff.

--- campbot.rb
+++ campbot.rb
@@ -4,6 +4,7 @@ class CampBot < Sinatra::Base
   end
   
   post '/' do
+    puts "Did receive post"
     Basecamp.establish_connection!(ENV['BASECAMP_URL'], ENV['BASECAMP_LOGIN'], ENV['BASECAMP_PASSWORD'])
     push = JSON.parse(params[:payload])
     halt 400 if push['repository']['url'] != ENV['GITHUB_REPO']
