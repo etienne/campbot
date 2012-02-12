7028760543b92b51fb0a2352acf2d95e44dd1d75
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-12 13:38:08 -0500

    Rename push variable to payload. More debug stuff.

--- campbot.rb
+++ campbot.rb
@@ -5,10 +5,10 @@ class CampBot < Sinatra::Base
   
   post '/' do
     Basecamp.establish_connection!(ENV['BASECAMP_URL'], ENV['BASECAMP_LOGIN'], ENV['BASECAMP_PASSWORD'], true)
-    push = JSON.parse(params[:payload])
-    puts "----------------- repo url = #{push['repository']['url']}"
-    halt 400 if push['repository']['url'] != ENV['GITHUB_REPO']
-    push['commits'].each do |commit|
+    payload = JSON.parse(params[:payload])
+    halt 400, '' if payload['repository']['url'] != ENV['GITHUB_REPO']
+    puts payload['commits'].inspect
+    payload['commits'].each do |commit|
       puts "Commit."
       if commit['message'] =~ /(fixes?|closes?) (\d+)/
         puts "Matching commit."
