aec086a834782b717c588aca081bc74dfc1be045
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-12 12:19:23 -0500

    Make the thing work, hopefully.

--- campbot.rb
+++ campbot.rb
@@ -1,5 +1,17 @@
 class CampBot < Sinatra::Base
   get '/' do
-    "Hello World!"
+    "Nothing to see here."
+  end
+  
+  post '/' do
+    Basecamp.establish_connection!(ENV['BASECAMP_URL'], ENV['BASECAMP_LOGIN'], ENV['BASECAMP_PASSWORD'])
+    push = JSON.parse(params[:payload])
+    halt 400 if push[:repository][:url] != ENV['GITHUB_REPO']
+    push[:commits].each do |commit|
+      if commit[:message] =~ /(fixes?|closes?) (\d+)/
+        t = Basecamp::TodoItem.find($1)
+        t.complete!
+      end
+    end
   end
 end
